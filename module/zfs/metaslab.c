tssge/zfs

Forward port changes back from FreeBSD branch.

module/zfs/metaslab.c
@@ -336,6 +336,16 @@ metaslab_class_get_dspace(metaslab_class_t *mc)
 }
 
 void
+metaslab_class_force_discard(metaslab_class_t *mc)
+{
+
+	mc->mc_alloc = 0;
+	mc->mc_deferred = 0;
+	mc->mc_space = 0;
+	mc->mc_dspace = 0;
+}
+
+void
 metaslab_class_histogram_verify(metaslab_class_t *mc)
 {
 	spa_t *spa = mc->mc_spa;
@@ -1578,6 +1588,19 @@ metaslab_fini(metaslab_t *msp)
 	metaslab_group_remove(mg, msp);
 
 	mutex_enter(&msp->ms_lock);
+	if (spa_exiting_any(mg->mg_vd->vdev_spa)) {
+		/* Catch-all cleanup as required for force export. */
+		range_tree_vacate(msp->ms_allocatable, NULL, NULL);
+		range_tree_vacate(msp->ms_freeing, NULL, NULL);
+		range_tree_vacate(msp->ms_freed, NULL, NULL);
+		range_tree_vacate(msp->ms_checkpointing, NULL, NULL);
+		for (int t = 0; t < TXG_SIZE; t++)
+			range_tree_vacate(msp->ms_allocating[t], NULL, NULL);
+		for (int t = 0; t < TXG_DEFER_SIZE; t++)
+			range_tree_vacate(msp->ms_defer[t], NULL, NULL);
+		msp->ms_deferspace = 0;
+	}
+
 	VERIFY(msp->ms_group == NULL);
 	metaslab_space_update(vd, mg->mg_class,
 	    -space_map_allocated(msp->ms_sm), 0, -msp->ms_size);
@@ -2379,6 +2402,28 @@ metaslab_sync(metaslab_t *msp, uint64_t txg)
 	ASSERT3P(msp->ms_checkpointing, !=, NULL);
 
 	/*
+	 * The pool is being forcibly exported.  Just discard everything.
+	 */
+	if (spa_exiting_any(spa)) {
+		mutex_enter(&msp->ms_sync_lock);
+		mutex_enter(&msp->ms_lock);
+		range_tree_vacate(alloctree, NULL, NULL);
+		range_tree_vacate(msp->ms_freeing, NULL, NULL);
+		range_tree_vacate(msp->ms_freed, NULL, NULL);
+		range_tree_vacate(msp->ms_checkpointing, NULL, NULL);
+		range_tree_vacate(msp->ms_allocating[txg & TXG_MASK],
+		    NULL, NULL);
+		range_tree_vacate(msp->ms_allocating[TXG_CLEAN(txg) & TXG_MASK],
+		    NULL, NULL);
+		for (int t = 0; t < TXG_DEFER_SIZE; t++) {
+			range_tree_vacate(msp->ms_defer[t], NULL, NULL);
+		}
+		mutex_exit(&msp->ms_lock);
+		mutex_exit(&msp->ms_sync_lock);
+		return;
+	}
+
+	/*
 	 * Normally, we don't want to process a metaslab if there are no
 	 * allocations or frees to perform. However, if the metaslab is being
 	 * forced to condense and it's loaded, we need to let it through.
@@ -2390,7 +2435,7 @@ metaslab_sync(metaslab_t *msp, uint64_t txg)
 		return;
 
 
-	VERIFY(txg <= spa_final_dirty_txg(spa));
+	spa_verify_dirty_txg(spa, txg);
 
 	/*
 	 * The only state that can actually be changing concurrently with
@@ -2659,7 +2704,7 @@ metaslab_sync_done(metaslab_t *msp, uint64_t txg)
 	msp->ms_deferspace += defer_delta;
 	ASSERT3S(msp->ms_deferspace, >=, 0);
 	ASSERT3S(msp->ms_deferspace, <=, msp->ms_size);
-	if (msp->ms_deferspace != 0) {
+	if (msp->ms_deferspace != 0 && !spa_exiting_any(spa)) {
 		/*
 		 * Keep syncing this metaslab until all deferred frees
 		 * are back in circulation.

module/zfs/spa_misc.c
@@ -434,7 +434,7 @@ spa_config_lock_init(spa_t *spa)
 		spa_config_lock_t *scl = &spa->spa_config_lock[i];
 		mutex_init(&scl->scl_lock, NULL, MUTEX_DEFAULT, NULL);
 		cv_init(&scl->scl_cv, NULL, CV_DEFAULT, NULL);
-		refcount_create_untracked(&scl->scl_count);
+		refcount_create_tracked(&scl->scl_count);
 		scl->scl_writer = NULL;
 		scl->scl_write_wanted = 0;
 	}
@@ -453,45 +453,32 @@ spa_config_lock_destroy(spa_t *spa)
 	}
 }
 
-int
-spa_config_tryenter(spa_t *spa, int locks, void *tag, krw_t rw)
+static int
+spa_config_eval_flags(spa_t *spa, spa_config_flag_t flags)
 {
-	for (int i = 0; i < SCL_LOCKS; i++) {
-		spa_config_lock_t *scl = &spa->spa_config_lock[i];
-		if (!(locks & (1 << i)))
-			continue;
-		mutex_enter(&scl->scl_lock);
-		if (rw == RW_READER) {
-			if (scl->scl_writer || scl->scl_write_wanted) {
-				mutex_exit(&scl->scl_lock);
-				spa_config_exit(spa, locks & ((1 << i) - 1),
-				    tag);
-				return (0);
-			}
-		} else {
-			ASSERT(scl->scl_writer != curthread);
-			if (!refcount_is_zero(&scl->scl_count)) {
-				mutex_exit(&scl->scl_lock);
-				spa_config_exit(spa, locks & ((1 << i) - 1),
-				    tag);
-				return (0);
-			}
-			scl->scl_writer = curthread;
-		}
-		(void) refcount_add(&scl->scl_count, tag);
-		mutex_exit(&scl->scl_lock);
+	int error = 0;
+
+	if ((flags & SCL_FLAG_TRYENTER) != 0)
+		error = EAGAIN;
+	if (error == 0 && ((flags & SCL_FLAG_NOSUSPEND) != 0)) {
+		/* Notification given by zio_suspend(). */
+		mutex_enter(&spa->spa_suspend_lock);
+		error = spa_suspended(spa) ? EAGAIN : 0;
+		mutex_exit(&spa->spa_suspend_lock);
 	}
-	return (1);
+	return (error);
 }
 
-void
-spa_config_enter(spa_t *spa, int locks, void *tag, krw_t rw)
+int
+_spa_config_enter_flags(spa_t *spa, int locks, void *tag, krw_t rw,
+    spa_config_flag_t flags, const char *file, size_t line)
 {
+	int error = 0;
 	int wlocks_held = 0;
 
 	ASSERT3U(SCL_LOCKS, <, sizeof (wlocks_held) * NBBY);
 
-	for (int i = 0; i < SCL_LOCKS; i++) {
+	for (int i = 0; error == 0 && i < SCL_LOCKS; i++) {
 		spa_config_lock_t *scl = &spa->spa_config_lock[i];
 		if (scl->scl_writer == curthread)
 			wlocks_held |= (1 << i);
@@ -500,21 +487,53 @@ spa_config_enter(spa_t *spa, int locks, void *tag, krw_t rw)
 		mutex_enter(&scl->scl_lock);
 		if (rw == RW_READER) {
 			while (scl->scl_writer || scl->scl_write_wanted) {
+				error = spa_config_eval_flags(spa, flags);
+				if (error != 0)
+					break;
 				cv_wait(&scl->scl_cv, &scl->scl_lock);
 			}
 		} else {
 			ASSERT(scl->scl_writer != curthread);
 			while (!refcount_is_zero(&scl->scl_count)) {
+				error = spa_config_eval_flags(spa, flags);
+				if (error != 0)
+					break;
 				scl->scl_write_wanted++;
 				cv_wait(&scl->scl_cv, &scl->scl_lock);
 				scl->scl_write_wanted--;
 			}
-			scl->scl_writer = curthread;
+			if (error == 0)
+				scl->scl_writer = curthread;
 		}
-		(void) refcount_add(&scl->scl_count, tag);
+		if (error == 0)
+			(void) _refcount_add(&scl->scl_count, tag, file, line);
 		mutex_exit(&scl->scl_lock);
+
+		if (error != 0 && i > 0) {
+			/* Should never happen for classic spa_config_enter. */
+			ASSERT3U(flags & SCL_FLAG_NOSUSPEND, !=, 0);
+			spa_config_exit(spa, locks & ((1 << i) - 1), tag);
+		}
 	}
+
 	ASSERT3U(wlocks_held, <=, locks);
+	return (error);
+}
+
+void
+_spa_config_enter(spa_t *spa, int locks, void *tag, krw_t rw,
+    const char *file, size_t line)
+{
+	_spa_config_enter_flags(spa, locks, tag, rw, /*flags*/0, file, line);
+}
+
+int
+_spa_config_tryenter(spa_t *spa, int locks, void *tag, krw_t rw,
+    const char *file, size_t line)
+{
+
+	return (_spa_config_enter_flags(spa, locks, tag, rw,
+	    SCL_FLAG_TRYENTER, file, line) == 0);
 }
 
 void
@@ -663,7 +682,7 @@ spa_add(const char *name, nvlist_t *config, const char *altroot)
 	spa->spa_deadman_ziotime = MSEC2NSEC(zfs_deadman_ziotime_ms);
 	spa_set_deadman_failmode(spa, zfs_deadman_failmode);
 
-	refcount_create(&spa->spa_refcount);
+	refcount_create_tracked(&spa->spa_refcount);
 	spa_config_lock_init(spa);
 	spa_stats_init(spa);
 
@@ -837,11 +856,11 @@ spa_next(spa_t *prev)
  * have the namespace lock held.
  */
 void
-spa_open_ref(spa_t *spa, void *tag)
+_spa_open_ref(spa_t *spa, void *tag, const char *file, size_t line)
 {
 	ASSERT(refcount_count(&spa->spa_refcount) >= spa->spa_minref ||
 	    MUTEX_HELD(&spa_namespace_lock));
-	(void) refcount_add(&spa->spa_refcount, tag);
+	(void) _refcount_add(&spa->spa_refcount, tag, file, line);
 }
 
 /*
@@ -1244,7 +1263,7 @@ spa_vdev_exit(spa_t *spa, vdev_t *vd, uint64_t txg, int error)
  * Lock the given spa_t for the purpose of changing vdev state.
  */
 void
-spa_vdev_state_enter(spa_t *spa, int oplocks)
+_spa_vdev_state_enter(spa_t *spa, int oplocks, const char *file, size_t line)
 {
 	int locks = SCL_STATE_ALL | oplocks;
 
@@ -1261,11 +1280,11 @@ spa_vdev_state_enter(spa_t *spa, int oplocks)
 		int low = locks & ~(SCL_ZIO - 1);
 		int high = locks & ~low;
 
-		spa_config_enter(spa, high, spa, RW_WRITER);
+		_spa_config_enter(spa, high, spa, RW_WRITER, file, line);
 		vdev_hold(spa->spa_root_vdev);
-		spa_config_enter(spa, low, spa, RW_WRITER);
+		_spa_config_enter(spa, low, spa, RW_WRITER, file, line);
 	} else {
-		spa_config_enter(spa, locks, spa, RW_WRITER);
+		_spa_config_enter(spa, locks, spa, RW_WRITER, file, line);
 	}
 	spa->spa_vdev_locks = locks;
 }
@@ -1714,6 +1733,19 @@ uint64_t
 spa_syncing_txg(spa_t *spa)
 {
 	return (spa->spa_syncing_txg);
+}
+
+/*
+ * Verify that the requesting thread isn't dirtying a txg it's not supposed
+ * to be.  Normally, this must be spa_final_dirty_txg(), but if the pool is
+ * being force exported, no data will be written to stable storage anyway.
+ */
+void
+spa_verify_dirty_txg(spa_t *spa, uint64_t txg)
+{
+
+	if (spa->spa_killer == NULL)
+		VERIFY3U(txg, <=, spa_final_dirty_txg(spa));
 }
 
 /*
@@ -2319,6 +2351,12 @@ spa_maxblocksize(spa_t *spa)
 		return (SPA_OLD_MAXBLOCKSIZE);
 }
 
+boolean_t
+spa_exiting_any(spa_t *spa)
+{
+	return (spa->spa_killer != NULL);
+}
+
 /*
  * NB: must hold spa_namespace_lock or spa_evicting_os_lock if the result of
  *     this is critical.
@@ -2326,7 +2364,7 @@ spa_maxblocksize(spa_t *spa)
 boolean_t
 spa_exiting(spa_t *spa)
 {
-	return (spa->spa_killer != NULL && spa->spa_killer != curthread);
+	return (spa_exiting_any(spa) && spa->spa_killer != curthread);
 }
 
 int

catalogicsoftware/zfs-spp

module/zfs/metaslab.c
@@ -303,6 +303,16 @@ metaslab_class_get_dspace(metaslab_class_t *mc)
 }
 
 void
+metaslab_class_force_discard(metaslab_class_t *mc)
+{
+
+	mc->mc_alloc = 0;
+	mc->mc_deferred = 0;
+	mc->mc_space = 0;
+	mc->mc_dspace = 0;
+}
+
+void
 metaslab_class_histogram_verify(metaslab_class_t *mc)
 {
 	vdev_t *rvd = mc->mc_spa->spa_root_vdev;
@@ -1480,6 +1490,16 @@ metaslab_fini(metaslab_t *msp)
 	metaslab_group_remove(mg, msp);
 
 	mutex_enter(&msp->ms_lock);
+	if (spa_exiting_any(mg->mg_vd->vdev_spa)) {
+		/* Catch-all cleanup as required for force export. */
+		range_tree_vacate(msp->ms_tree, NULL, NULL);
+		range_tree_vacate(msp->ms_freeingtree, NULL, NULL);
+		range_tree_vacate(msp->ms_freedtree, NULL, NULL);
+		for (int t = 0; t < TXG_DEFER_SIZE; t++)
+			range_tree_vacate(msp->ms_defertree[t], NULL, NULL);
+		msp->ms_deferspace = 0;
+	}
+
 	VERIFY(msp->ms_group == NULL);
 	vdev_space_update(mg->mg_vd, -space_map_allocated(msp->ms_sm),
 	    0, -msp->ms_size);
@@ -2222,10 +2242,25 @@ metaslab_sync(metaslab_t *msp, uint64_t txg)
 	ASSERT3P(msp->ms_freedtree, !=, NULL);
 
 	/*
-	 * Normally, we don't want to process a metaslab if there
-	 * are no allocations or frees to perform. However, if the metaslab
-	 * is being forced to condense and it's loaded, we need to let it
-	 * through.
+	 * The pool is being forcibly exported.  Just discard everything.
+	 */
+	if (spa_exiting_any(spa)) {
+		mutex_enter(&msp->ms_lock);
+		range_tree_vacate(alloctree, NULL, NULL);
+		range_tree_vacate(msp->ms_tree, NULL, NULL);
+		range_tree_vacate(msp->ms_freeingtree, NULL, NULL);
+		range_tree_vacate(msp->ms_freedtree, NULL, NULL);
+		for (int t = 0; t < TXG_DEFER_SIZE; t++) {
+			range_tree_vacate(msp->ms_defertree[t], NULL, NULL);
+		}
+		mutex_exit(&msp->ms_lock);
+		return;
+	}
+
+	/*
+	 * Normally, we don't want to process a metaslab if there are no
+	 * allocations or frees to perform. However, if the metaslab is being
+	 * forced to condense and it's loaded, we need to let it through.
 	 */
 	if (range_tree_space(alloctree) == 0 &&
 	    range_tree_space(msp->ms_freeingtree) == 0 &&
@@ -2233,7 +2268,7 @@ metaslab_sync(metaslab_t *msp, uint64_t txg)
 		return;
 
 
-	VERIFY(txg <= spa_final_dirty_txg(spa));
+	spa_verify_dirty_txg(spa, txg);
 
 	/*
 	 * The only state that can actually be changing concurrently with
@@ -2448,7 +2483,7 @@ metaslab_sync_done(metaslab_t *msp, uint64_t txg)
 	msp->ms_deferspace += defer_delta;
 	ASSERT3S(msp->ms_deferspace, >=, 0);
 	ASSERT3S(msp->ms_deferspace, <=, msp->ms_size);
-	if (msp->ms_deferspace != 0) {
+	if (msp->ms_deferspace != 0 && !spa_exiting_any(spa)) {
 		/*
 		 * Keep syncing this metaslab until all deferred frees
 		 * are back in circulation.

module/zfs/spa_misc.c
@@ -366,7 +366,7 @@ spa_config_lock_init(spa_t *spa)
 		spa_config_lock_t *scl = &spa->spa_config_lock[i];
 		mutex_init(&scl->scl_lock, NULL, MUTEX_DEFAULT, NULL);
 		cv_init(&scl->scl_cv, NULL, CV_DEFAULT, NULL);
-		zfs_refcount_create_untracked(&scl->scl_count);
+		zfs_refcount_create_tracked(&scl->scl_count);
 		scl->scl_writer = NULL;
 		scl->scl_write_wanted = 0;
 	}
@@ -387,48 +387,33 @@ spa_config_lock_destroy(spa_t *spa)
 	}
 }
 
-int
-spa_config_tryenter(spa_t *spa, int locks, void *tag, krw_t rw)
+static int
+spa_config_eval_flags(spa_t *spa, spa_config_flag_t flags)
 {
-	int i;
+	int error = 0;
 
-	for (i = 0; i < SCL_LOCKS; i++) {
-		spa_config_lock_t *scl = &spa->spa_config_lock[i];
-		if (!(locks & (1 << i)))
-			continue;
-		mutex_enter(&scl->scl_lock);
-		if (rw == RW_READER) {
-			if (scl->scl_writer || scl->scl_write_wanted) {
-				mutex_exit(&scl->scl_lock);
-				spa_config_exit(spa, locks & ((1 << i) - 1),
-				    tag);
-				return (0);
-			}
-		} else {
-			ASSERT(scl->scl_writer != curthread);
-			if (!zfs_refcount_is_zero(&scl->scl_count)) {
-				mutex_exit(&scl->scl_lock);
-				spa_config_exit(spa, locks & ((1 << i) - 1),
-				    tag);
-				return (0);
-			}
-			scl->scl_writer = curthread;
-		}
-		(void) zfs_refcount_add(&scl->scl_count, tag);
-		mutex_exit(&scl->scl_lock);
+	if ((flags & SCL_FLAG_TRYENTER) != 0)
+		error = EAGAIN;
+	if (error == 0 && ((flags & SCL_FLAG_NOSUSPEND) != 0)) {
+		/* Notification given by zio_suspend(). */
+		mutex_enter(&spa->spa_suspend_lock);
+		error = spa_suspended(spa) ? EAGAIN : 0;
+		mutex_exit(&spa->spa_suspend_lock);
 	}
-	return (1);
+	return (error);
 }
 
-void
-spa_config_enter(spa_t *spa, int locks, void *tag, krw_t rw)
+int
+_spa_config_enter_flags(spa_t *spa, int locks, void *tag, krw_t rw,
+    spa_config_flag_t flags, const char *file, size_t line)
 {
+	int error = 0;
 	int wlocks_held = 0;
 	int i;
 
 	ASSERT3U(SCL_LOCKS, <, sizeof (wlocks_held) * NBBY);
 
-	for (i = 0; i < SCL_LOCKS; i++) {
+	for (i = 0; error == 0 && i < SCL_LOCKS; i++) {
 		spa_config_lock_t *scl = &spa->spa_config_lock[i];
 		if (scl->scl_writer == curthread)
 			wlocks_held |= (1 << i);
@@ -437,21 +422,53 @@ spa_config_enter(spa_t *spa, int locks, void *tag, krw_t rw)
 		mutex_enter(&scl->scl_lock);
 		if (rw == RW_READER) {
 			while (scl->scl_writer || scl->scl_write_wanted) {
+				error = spa_config_eval_flags(spa, flags);
+				if (error != 0)
+					break;
 				cv_wait(&scl->scl_cv, &scl->scl_lock);
 			}
 		} else {
 			ASSERT(scl->scl_writer != curthread);
 			while (!zfs_refcount_is_zero(&scl->scl_count)) {
+				error = spa_config_eval_flags(spa, flags);
+				if (error != 0)
+					break;
 				scl->scl_write_wanted++;
 				cv_wait(&scl->scl_cv, &scl->scl_lock);
 				scl->scl_write_wanted--;
 			}
-			scl->scl_writer = curthread;
+			if (error == 0)
+				scl->scl_writer = curthread;
 		}
-		(void) zfs_refcount_add(&scl->scl_count, tag);
+		if (error == 0)
+			(void) _zfs_refcount_add(&scl->scl_count, tag, file, line);
 		mutex_exit(&scl->scl_lock);
+
+		if (error != 0 && i > 0) {
+			/* Should never happen for classic spa_config_enter. */
+			ASSERT3U(flags & SCL_FLAG_NOSUSPEND, !=, 0);
+			spa_config_exit(spa, locks & ((1 << i) - 1), tag);
+		}
 	}
-	ASSERT(wlocks_held <= locks);
+
+	ASSERT3U(wlocks_held, <=, locks);
+	return (error);
+}
+
+void
+_spa_config_enter(spa_t *spa, int locks, void *tag, krw_t rw,
+    const char *file, size_t line)
+{
+	_spa_config_enter_flags(spa, locks, tag, rw, /*flags*/0, file, line);
+}
+
+int
+_spa_config_tryenter(spa_t *spa, int locks, void *tag, krw_t rw,
+    const char *file, size_t line)
+{
+
+	return (_spa_config_enter_flags(spa, locks, tag, rw,
+	    SCL_FLAG_TRYENTER, file, line) == 0);
 }
 
 void
@@ -603,7 +620,7 @@ spa_add(const char *name, nvlist_t *config, const char *altroot)
 
 	spa->spa_deadman_synctime = MSEC2NSEC(zfs_deadman_synctime_ms);
 
-	zfs_refcount_create(&spa->spa_refcount);
+	zfs_refcount_create_tracked(&spa->spa_refcount);
 	spa_config_lock_init(spa);
 	spa_stats_init(spa);
 
@@ -765,11 +782,11 @@ spa_next(spa_t *prev)
  * have the namespace lock held.
  */
 void
-spa_open_ref(spa_t *spa, void *tag)
+_spa_open_ref(spa_t *spa, void *tag, const char *file, size_t line)
 {
 	ASSERT(zfs_refcount_count(&spa->spa_refcount) >= spa->spa_minref ||
 	    MUTEX_HELD(&spa_namespace_lock));
-	(void) zfs_refcount_add(&spa->spa_refcount, tag);
+	(void) _zfs_refcount_add(&spa->spa_refcount, tag, file, line);
 }
 
 /*
@@ -1169,7 +1186,7 @@ spa_vdev_exit(spa_t *spa, vdev_t *vd, uint64_t txg, int error)
  * Lock the given spa_t for the purpose of changing vdev state.
  */
 void
-spa_vdev_state_enter(spa_t *spa, int oplocks)
+_spa_vdev_state_enter(spa_t *spa, int oplocks, const char *file, size_t line)
 {
 	int locks = SCL_STATE_ALL | oplocks;
 
@@ -1186,11 +1203,11 @@ spa_vdev_state_enter(spa_t *spa, int oplocks)
 		int low = locks & ~(SCL_ZIO - 1);
 		int high = locks & ~low;
 
-		spa_config_enter(spa, high, spa, RW_WRITER);
+		_spa_config_enter(spa, high, spa, RW_WRITER, file, line);
 		vdev_hold(spa->spa_root_vdev);
-		spa_config_enter(spa, low, spa, RW_WRITER);
+		_spa_config_enter(spa, low, spa, RW_WRITER, file, line);
 	} else {
-		spa_config_enter(spa, locks, spa, RW_WRITER);
+		_spa_config_enter(spa, locks, spa, RW_WRITER, file, line);
 	}
 	spa->spa_vdev_locks = locks;
 }
@@ -1623,6 +1640,19 @@ uint64_t
 spa_syncing_txg(spa_t *spa)
 {
 	return (spa->spa_syncing_txg);
+}
+
+/*
+ * Verify that the requesting thread isn't dirtying a txg it's not supposed
+ * to be.  Normally, this must be spa_final_dirty_txg(), but if the pool is
+ * being force exported, no data will be written to stable storage anyway.
+ */
+void
+spa_verify_dirty_txg(spa_t *spa, uint64_t txg)
+{
+
+	if (spa->spa_killer == NULL)
+		VERIFY3U(txg, <=, spa_final_dirty_txg(spa));
 }
 
 /*
@@ -2096,6 +2126,12 @@ spa_maxblocksize(spa_t *spa)
 		return (SPA_OLD_MAXBLOCKSIZE);
 }
 
+boolean_t
+spa_exiting_any(spa_t *spa)
+{
+	return (spa->spa_killer != NULL);
+}
+
 /*
  * NB: must hold spa_namespace_lock or spa_evicting_os_lock if the result of
  *     this is critical.
@@ -2103,7 +2139,7 @@ spa_maxblocksize(spa_t *spa)
 boolean_t
 spa_exiting(spa_t *spa)
 {
-	return (spa->spa_killer != NULL && spa->spa_killer != curthread);
+	return (spa_exiting_any(spa) && spa->spa_killer != curthread);
 }
 
 int
@@ -2196,13 +2232,13 @@ EXPORT_SYMBOL(spa_remove);
 EXPORT_SYMBOL(spa_next);
 
 /* Refcount functions */
-EXPORT_SYMBOL(spa_open_ref);
+EXPORT_SYMBOL(_spa_open_ref);
 EXPORT_SYMBOL(spa_close);
 EXPORT_SYMBOL(spa_refcount_zero);
 
 /* Pool configuration lock */
-EXPORT_SYMBOL(spa_config_tryenter);
-EXPORT_SYMBOL(spa_config_enter);
+EXPORT_SYMBOL(_spa_config_tryenter);
+EXPORT_SYMBOL(_spa_config_enter);
 EXPORT_SYMBOL(spa_config_exit);
 EXPORT_SYMBOL(spa_config_held);
 
@@ -2211,7 +2247,7 @@ EXPORT_SYMBOL(spa_vdev_enter);
 EXPORT_SYMBOL(spa_vdev_exit);
 
 /* Pool vdev state change lock */
-EXPORT_SYMBOL(spa_vdev_state_enter);
+EXPORT_SYMBOL(_spa_vdev_state_enter);
 EXPORT_SYMBOL(spa_vdev_state_exit);
 
 /* Accessor functions */