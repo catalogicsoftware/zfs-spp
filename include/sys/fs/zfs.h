wca/ZoF

zfs: add zpool ddtload subcommand
Implement an ioctl which interfaces with a DDT adapter callback for
loading all entries of a given DDT object type, and calls it for every
DDT object that exists in a pool.

Implement the ZAP adapter callback by prefetching the entire zap object.

This subcommand enables users to pre-warm (or re-warm) the cache for DDT
entries if they reboot or otherwise perform an export/import cycle, and
skip the wait for the entries to be loaded, to restore normal I/O write
performance to the pool.

Signed-off-by:	Will Andrews <will@firepipe.net>

include/sys/fs/zfs.h
@@ -1278,6 +1278,7 @@ typedef enum zfs_ioc {
 	ZFS_IOC_REDACT,				/* 0x5a51 */
 	ZFS_IOC_GET_BOOKMARK_PROPS,		/* 0x5a52 */
 	ZFS_IOC_WAIT,				/* 0x5a53 */
+	ZFS_IOC_POOL_DDTLOAD,			/* 0x5a54 */
 
 	/*
 	 * Linux - 3/64 numbers reserved.

catalogicsoftware/zfs-spp

Conflicts:
	cmd/zpool/zpool_main.c
	include/libzfs_core.h
	include/sys/fs/zfs.h
	man/man8/zpool.8

include/sys/fs/zfs.h
@@ -1325,6 +1325,7 @@ typedef enum zfs_ioc {
 	ZFS_IOC_GET_BOOKMARK_PROPS,		/* 0x5a52 */
 	ZFS_IOC_WAIT,				/* 0x5a53 */
 	ZFS_IOC_WAIT_FS,			/* 0x5a54 */
+	ZFS_IOC_POOL_DDTLOAD,			/* 0x5a55 */
 
 	/*
 	 * Per-platform (Optional) - 8/128 numbers reserved.